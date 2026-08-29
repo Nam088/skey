acdc0e9146cbfcf1de4706afacd764b237a919ec
Nam088 <nam077.work@gmail.com>
2026-08-29 19:15:48 +0700

    fix(cask): simplify cask format and comply with strict Homebrew style

--- Casks/skey.rb
+++ Casks/skey.rb
@@ -1,21 +1,10 @@
 cask "skey" do
   version "1.0.16"
+  sha256 "db3f5e66aa10c71c506a08b1c88d5fc24bbc077b4a5c51611425665e5a24ad77"
 
-  # CASK_ARCH_URLS_START
-  on_arm do
-    sha256 "db3f5e66aa10c71c506a08b1c88d5fc24bbc077b4a5c51611425665e5a24ad77"
-
-    url "https://github.com/Nam088/skey/releases/download/v1.0.16/SKey-Installer.dmg"
-  end
-  on_intel do
-    sha256 "db3f5e66aa10c71c506a08b1c88d5fc24bbc077b4a5c51611425665e5a24ad77"
-
-    url "https://github.com/Nam088/skey/releases/download/v1.0.16/SKey-Installer.dmg"
-  end
-  # CASK_ARCH_URLS_END
-
+  url "https://github.com/Nam088/skey/releases/download/v#{version}/SKey-Installer.dmg"
   name "SKey"
-  desc "Modern, high-performance Vietnamese input engine & utilities for macOS"
+  desc "Modern, high-performance Vietnamese input engine and utilities"
   homepage "https://github.com/Nam088/skey"
 
   depends_on macos: :sonoma
